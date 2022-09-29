853e71fa7d7fe044c75e0482779a28069de53307
boxheadzero <o_kung123@hotmail.com>
2022-09-29 16:10:43 +0700

    values and variables => 7 type of values, data types => assign data to variables, let const and var => let means reusable /const means unreusable /var means same as let but used in older ECMAScript, Basic Operators => arithmetics /comparison /precedence

---
+++ js-course/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,40 @@
+// Values and Variables
+const country = "Thailand";
+const continent = "Asia";
+let population = 70.2;
+
+console.log("---Below this are assignments 01---");
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Data Types
+const isIsland = false;
+let language;
+
+console.log("---Below this are assignments 02---");
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// let, const and var
+language = 'Thai';
+
+console.log("---Below this are assignments 03---");
+console.log(isIsland);
+console.log(language);
+
+//Basic Operators
+let halfPopulation = population / 2;
+population++;
+let finlandPopulation = 6;
+let avgPopulation = 33;
+const description = "Portugal in Europe, and its 11 million people speak portuguese";
+
+console.log("---Below this are assignments 04---");
+console.log(halfPopulation);
+console.log(population);
+console.log(finlandPopulation > population);
+console.log(population < avgPopulation);
+console.log(description);
---
+++ js-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,83 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 18 - 25);
+
+let firstName = 'Oti';
+
+console.log(firstName);
+
+let oti_design = "Oti.Design";
+let $function = 28;
+
+let person = "Oti";
+let PI = 3.1415;
+
+let myFirstJob = "Officer";
+let myGoalJob = "Entrepeneur";
+
+let job1 = "officer";
+let job2 = "entrepeneur";
+
+console.log(myFirstJob);
+console.log("to be");
+console.log(myGoalJob);
+console.log("Credit " + oti_design);
+
+// Use let to declare first a new valuable
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Oti");
+
+javascriptIsFun = "!YES";
+let year;
+
+console.log(typeof javascriptIsFun);
+console.log(typeof year);
+
+year = 1996;
+console.log(typeof year);
+console.log(typeof null);
+
+//reusable valuable pick let
+let age = 25;
+age = 26;
+
+//fixed valuable pick const
+const birthYear = 1996;
+
+//not modern ECMAscript pick var
+var job = 'officer';
+job = 'entrepeneur';
+
+lastName = 'KYTH';
+console.log(lastName);
+
+// operate in grobal scope
+
+const currentYear = 2022;
+const ageOti = currentYear - 1996;
+const ageCompany = currentYear - 2018;
+console.log(ageOti, ageCompany);
+
+console.log(ageOti * 1.1, ageCompany / 1, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+firstName = 'Oti';
+lastName = 'Arayadechkajorn';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // 101
+x--;// 100
+console.log(x);
+
+// Comparison operators <, >, >=, <=
+ageOti > ageCompany; // true
+ageOti < ageCompany; // false
+ageOti >= 25 // true
+
