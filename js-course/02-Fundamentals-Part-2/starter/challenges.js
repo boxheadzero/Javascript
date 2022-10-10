f96fa00903611055b8084dd7884fe3bc88507e0c
boxheadzero <o_kung123@hotmail.com>
2022-10-10 22:33:05 +0700

    array, object, looping arrays and breaking / continuing

--- js-course/02-Fundamentals-Part-2/starter/challenges.js
+++ js-course/02-Fundamentals-Part-2/starter/challenges.js
@@ -19,3 +19,67 @@ checkWinner(scoreDolphins, scoreKoalas);
 scoreDolphins = calcAverage(85, 54, 41);
 scoreKoalas = calcAverage(23, 34, 27);
 checkWinner(scoreDolphins, scoreKoalas);
+
+//Challenge #2
+
+const bills = [125, 555, 44];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    
+    for (let i = 0; i < bill.length; i++) {
+        if (bill[i] > 50 && bill[i] < 300) {
+            tipValue = bill[i] * 0.15;
+            tips.push(tipValue);
+            totals.push(tips[i] + bill[i]);
+
+        } else {
+            tipValue = bill[i] * 0.20;
+            tips.push(tipValue);
+            totals.push(tips[i] + bill[i]);
+        }
+    }
+    return tips, totals;
+}
+
+console.log(bills);
+console.log(calcTip(bills));
+console.log(tips);
+console.log(totals);
+
+//Challenge #3
+const data1 = {
+    firstName: 'Mark',
+    lastName: 'Miller',
+    fullName: this.firstName + this.lastName,
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2;
+        return this.BMI;
+    }
+};
+
+const data2 = {
+    firstName: 'John',
+    lastName: 'Smith',
+    fullName: this.firstName + this.lastName,
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.BMI = this.mass / this.height ** 2;
+        return this.BMI;
+    }
+};
+
+function checkBMI() {
+    return data1.BMI > data2.BMI ? `${data1.firstName}'s BMI (${Math.round(data1.BMI * 10) / 10}) is higher than ${data2.firstName}'s BMI (${Math.round(data2.BMI * 10) / 10})!` : `${data2.firstName}'s BMI (${Math.round(data2.BMI * 10) / 10}) is higher than ${data1.firstName}'s BMI (${Math.round(data1.BMI * 10) / 10})!`
+};
+
+data1.calcBMI();
+data2.calcBMI();
+console.log(checkBMI());
+
--- js-course/02-Fundamentals-Part-2/starter/script.js
+++ js-course/02-Fundamentals-Part-2/starter/script.js
@@ -70,7 +70,7 @@ function fruitProcessor(apples, oranges) {
 
 console.log(fruitProcessor(2, 3)); */
 
-const currentYear = 2022;
+/* const currentYear = 2022;
 
  function yearUntilRetirement(birthYear, firstName) {
     const age = currentYear - birthYear;
@@ -89,5 +89,157 @@ const currentYear = 2022;
 yearUntilRetirement(1996, 'Oti');
 yearUntilRetirement(1991, 'Jonas');
 yearUntilRetirement(1959, 'House');
-yearUntilRetirement(2005, 'Junior');
+yearUntilRetirement(2005, 'Junior'); */
 
+/* // Array
+const friends = ['Boss', 'Prince', 'Golf', 'Name', 'Win'];
+
+const years = new Array(1996, 1984, 2008, 2020);
+
+friends[2] = 'Jordan';
+console.log(friends);
+
+const firstName = 'Oti';
+const oti = [firstName, 'Design', 2025 - 1996, 'be entrepeneur', friends];
+console.log(oti);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2025 - birthYear;
+}
+const years1 = [1996, 1954, 2002, 2010, 2011];
+
+const age1 = calcAge(years1[0]);
+const age2 = calcAge(years1[1]);
+const age3 = calcAge(years1[years1.length - 1]);
+
+const ages = [calcAge(years1[0]), calcAge(years1[1]), calcAge(years1[years1.length-1])];
+
+const friends1 = ['A', 'B', 'C'];
+//Add elements
+const newlength = friends1.push('D'); // ['A', 'B', 'C', 'D']
+friends1.unshift('a'); // ['a', 'A', 'B', 'C', 'D']
+
+//Remove elements
+friends1.pop(); // last
+const popped = friends1.pop();
+
+friends1.shift(); // first
+
+friends1.indexOf('A'); // 1
+friends1.indexOf('b'); // -1
+
+// Boolean
+friends1.includes('a'); // true
+friends1.includes('b'); // false
+
+if (friends1.includes('A')) {
+    console.log('You have a A');
+} */
+
+// Object
+/* const otiArray = [
+    'Oti',
+    'Design',
+    2025 - 1996,
+    'Entrepeneur',
+    ['Pirnce', 'Boss', 'Golf']
+];
+
+const oti = {
+    firstName: 'Oti',
+    lastName: 'Design',
+    age: 2025 - 1996,
+    job: 'Design',
+    friends: ['Prince', 'Boss', 'Golf']
+};
+
+console.log(oti.age);
+console.log(oti['age']);
+
+const nameKey = 'Name';
+console.log(oti['first' + nameKey]);
+console.log(oti['last' + nameKey]);
+
+//Adaptive
+const interestedIn = prompt('What do you want to know about Oti? Choose between firstName, lastName, age, job and friends');
+
+if (oti[interestedIn]) {
+    console.log(oti[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+}
+
+// Challenge
+console.log(`${oti.firstName} has ${oti.friends.length}, and his best friend is called ${oti.friends[0]}`); */
+
+/* const oti = {
+    firstName: 'Oti',
+    lastName: 'Design',
+    birthYear: 2010,
+    job: 'Entrepeneur',
+    friends: ['Prince', 'Boss', 'Golf'],
+    hasDriversLicense: true,
+
+    // calcAge: function (birthYear) {
+    //     return 2025 - birthYear;
+    // }
+
+    // calcAge: function () {
+    //     console.log(this);
+    //     return 2025 - this.birthYear;
+    // }
+
+    calcAge: function () {
+        this.age = 2025 - this.birthYear;
+        return this.age;
+    },
+
+    checkDriversLicense: function () {
+        return this.age < 18 ? this[hasDriversLicense = true] : this[hasDriversLicense = false];
+    },
+
+    getSummary: function () {
+        return `${this.firstName} is a ${this.age}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+
+console.log(oti.calcAge());
+console.log(oti.checkDriversLicense());
+console.log(oti.age);
+console.log(oti.hasDriversLicense);
+console.log(oti.getSummary()); // must be fix! */
+
+// Looping Arrays, Breaking and Continuing
+
+const oti = {
+    firstName: 'Oti',
+    lastName: 'Design',
+    birthYear: 2010,
+    job: 'Entrepeneur',
+    friends: ['Prince', 'Boss', 'Golf'],
+    hasDriversLicense: true,
+};
+
+const years = [1991, 2007, 2001, 1921];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+    ages.push(2025 - years[i]);
+}
+console.log(ages);
+
+// continuing and break
+console.log('---String---');
+for (let i = 0; i < oti.length; i++) {
+    if (typeof oti[i] !== 'string') continue;
+
+    console.log(oti[i], typeof oti[i]);
+}
+
+console.log('---Break with number');
+for (let i = 0; i < oti.length; i++) {
+    if (typeof oti[i] === 'number') break;
+
+    console.log(oti[i], typeof oti[i]);
+}
