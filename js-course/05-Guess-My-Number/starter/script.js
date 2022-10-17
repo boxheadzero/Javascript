2b13b29754031e11372537f7c000573f5b63158b
boxheadzero <o_kung123@hotmail.com>
2022-10-17 17:10:17 +0700

    Project #1 (Guess My Number Game) has done => DOM manipulation

--- js-course/05-Guess-My-Number/starter/script.js
+++ js-course/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+let secretNumber = Number(
+  Math.trunc(Math.random(document.querySelector('.number')) * 20) + 1
+);
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When there is no input
+  if (!guess) {
+    displayMessage('⛔ No number!');
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    if (score === 20) {
+      displayMessage('🎊 Perfect 🎊');
+    } else {
+      displayMessage('🎊 Correct');
+    }
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When player guess number to high
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 too high' : '📉 too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('😫 Game over');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  secretNumber = Number(
+    Math.trunc(Math.random(document.querySelector('.number')) * 20) + 1
+  ); // put new data back
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
